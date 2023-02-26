b93d0f5df2b0bfb6111d70f0781dcc7f751078f6
unknown <mouni.shalaby@gmail.com>
2023-02-26 17:54:20 +0200

    fetch Data

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -4,7 +4,33 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
