16cc657b69ace412d0324b549e9ea03a7a5a570c
unknown <mouni.shalaby@gmail.com>
2023-03-04 11:59:08 +0200

    add all button and the rest of other  Buttons manually

--- 05-menu/src/Categories.js
+++ 05-menu/src/Categories.js
@@ -6,6 +6,15 @@ const Categories = ({ filterItems }) => {
       <button className="filter-btn" onClick={() => filterItems("breakfast")}>
         breakfast
       </button>
+      <button className="filter-btn" onClick={() => filterItems("lunch")}>
+        lunch
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("shakes")}>
+        shakes
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("all")}>
+        all
+      </button>
     </div>
   );
 };
